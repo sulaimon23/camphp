b30e60adedbaf47bb37fbdfad2e685f3f84ba45e
sulaimon23 <tsulaimon21@yahoo.com>
2021-03-01 17:04:49 +0100

    olo

--- files/contact-form-handler.php
+++ files/contact-form-handler.php
@@ -15,7 +15,7 @@ if(isset($_POST['submit'])){
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     // echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    header('Location: third.php');
+    header('Location: third.html');
     // You cannot use header and echo together. It's one or the other.
     }
 ?>
